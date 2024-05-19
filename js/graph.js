ee2681e288a94fe44353dabfe1f084c04a56821f
Flavio Valle Da Cunha <88721496+flavio-valle@users.noreply.github.com>
2024-05-19 01:07:34 -0300

    add chart js

--- js/graph.js
+++ js/graph.js
@@ -1,1 +1,25 @@
-console.log("funcionou")
+
+const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [1, 1, 1, 1, 2, 3],
+        borderWidth: 2,
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+
+  const ctx2 = document.getElementById('myChart2');
+
+    
